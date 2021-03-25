b8e3f7ad7798891949802dc2bf1f4ecd971af5a4
Edgar Silva <23esms23@gmail.com>
2021-03-25 10:51:51 +0000

    study mappings component and tree mod methods

---
+++ src/components/StudyManagement/StudyMappings.vue
@@ -0,0 +1,302 @@
+<template>
+    <v-content>
+        <v-container fluid>
+        <v-layout v-if="current_study.mappings" align-start justify-center>
+            <div v-for="scope in tree_list" :key="scope.scope">
+            <v-list two-line :color="color">
+                <v-subheader dark>
+                {{scope.scope}}
+                </v-subheader>
+                <v-treeview-draggable v-model="scope.tree" group="mappings" :scope="scope.scope" :add_leaf="add_leaf" :remove_leaf="remove_leaf"></v-treeview-draggable>
+            </v-list>
+            </div>
+        </v-layout>
+        </v-container>
+    </v-content>
+</template>
+
+<script>
+import VTreeviewDraggable from "../DraggableTreeview/VTreeviewDraggable"
+const axios = require('axios')
+    export default {
+        name: 'StudyMappings',
+        components: {
+            VTreeviewDraggable,
+        },
+        data() {
+            return {
+                tree_list: [],
+                scopes: null,
+                classes: null,
+                fields: null,
+                color: "#3A3A3A",
+            }
+        },
+        props: {
+            current_study: {
+                type: Object,
+                default: function() { return {} }
+            }
+        },
+        methods: {
+            /**
+             * Requests and retreives all scopes, placing the response in scopes
+             */
+            get_scopes() {
+                let url = process.env.VUE_APP_MAPPINGS_BASE_PATH + 'scopes'
+                axios.get(url)
+                .then((response) => {
+                    this.scopes = response.data
+                })
+                .catch(() => {
+                    this.scopes = []
+                })
+            },
+            /**
+             * Requests and retreives all classes, placing the response in classes
+             */
+            get_classes () {
+                let url = process.env.VUE_APP_MAPPINGS_BASE_PATH + 'class'
+                axios.get(url)
+                .then((response) => {
+                    this.classes = response.data
+                })
+                .catch(() => {
+                    this.classes = []
+                })
+            },
+            /**
+             * Requests and retreives all fields, placing the response in fields
+             */
+            get_fields() {
+                let url = process.env.VUE_APP_MAPPINGS_BASE_PATH + 'fields'
+                axios.get(url)
+                .then((response) => {
+                    this.fields = response.data
+                })
+                .catch(() => {
+                    this.fields = []
+                })
+            },
+            /**
+             * Fills list of trees (tree_list) with mappings from current study (current_study).
+             * For each class adds attribute is_open to force open on page load
+             * Uses add_leaf to actually insert each node in the tree
+             * Final list of trees consists of 1 object per scope with corresponding mappings tree
+             */
+            new_fields_tree() {
+                this.tree_list = []
+                let field_counter = 0
+                for (let i = 0; i < this.current_study.mappings.length; i++) {
+                    let mapping = this.current_study.mappings[i]
+                    let scope_name = this.get_scope_name(mapping.scope)
+
+                    let tree_obj = {
+                        scope_id: mapping.scope,
+                        scope: scope_name,
+                        tree: []
+                    }
+
+                    for (let j = 0; j < this.classes.length; j++) {
+                        let class_name = this.classes[j].name.toLowerCase()
+                        tree_obj.tree.push({
+                            id: scope_name + '.' + class_name,
+                            is_open: true,
+                            name: class_name,
+                            children: []
+                        })
+                    }
+
+                    let fields_names = this.get_fields_names_with_classes(mapping.fields)
+
+                    for (let j = 0; j < fields_names.length; j++) {
+                        let small_name_split = fields_names[j].split('.')
+                        let small_name = small_name_split[small_name_split.length - 1]
+                        let field_obj = {
+                            id: field_counter,
+                            name: small_name,
+                            field_name: fields_names[j]
+                        }
+                        field_counter++
+                        this.add_leaf(tree_obj.tree, scope_name, field_obj)
+                    }
+
+                    this.tree_list.push(tree_obj)
+                }
+            },
+            /**
+             * Searches scopes by id and returns scope name
+             * @param id Requesting scope id
+             * @return Scope name
+             */
+            get_scope_name (id) {
+                for (let i = 0; i < this.scopes.length; i++) {
+                    if (this.scopes[i].id == id) {
+                        return this.scopes[i].name
+                    }
+                }
+            },
+            /**
+             * Creates array with fields names concatenated with corresponding class and a dot
+             * Example: if class = 'class_a' and field name = 'field_a' function returns 'class_a.field_a'
+             * @param ids Array of ids to get names from
+             * @return Array of names
+             */
+            get_fields_names_with_classes (ids) {
+                var fields_names = []
+                for (let i = 0; i < ids.length; i++) {
+                    for (let j = 0; j < this.fields.length; j++) {
+                        if (ids[i] == this.fields[j].id) {
+                            fields_names.push(this.get_class_name(this.fields[j].class_id).toLowerCase() + '.' + this.fields[j].name)
+                            break
+                        }
+                    }
+                }
+                return fields_names
+            },
+            /**
+             * Searches classes by id and returns class name
+             * @param id Requesting class id
+             * @return Class name
+             */
+            get_class_name (id) {
+                for (let i = 0; i < this.classes.length; i++) {
+                    if (this.classes[i].id == id) {
+                        return this.classes[i].name
+                    }
+                }
+            },
+            /**
+             * Adds new element to a tree (localValue).
+             * If the element is a leaf its id is numerical
+             * If the element is a parent its id is a concatenation of the corresponding scope and "parent" name
+             * If the element is a leaf and parents do not exist, any and all non-existing parents are created
+             * @param localValue Tree where element is being added to
+             * @param scope Scope name corresponding to tree
+             * @param element Element being added to the tree (localValue)
+             */
+            add_leaf: function(localValue, scope, element) {
+                let split
+                let new_id
+                if (Object.prototype.hasOwnProperty.call(element, 'field_name')) {
+                    split = element.field_name.split('.')
+                    new_id = null
+                } else {
+                    split = element.id.split('.')
+                    split.splice(0, 1)
+                    new_id = scope + '.' + split.join('.')
+                }
+                let current_name = scope + '.' + split[0]
+                let current_block = localValue[localValue.findIndex(v => v.id == current_name)]
+
+                for (let i = 1; i < split.length - 1; i++) {
+                    current_name += '.' + split[i]
+                    let index = current_block.children.findIndex(v => v.id == current_name)
+
+                    if (index == -1) {
+                        let new_parent = {
+                            id: current_name,
+                            name: split[i],
+                            children: []
+                        }
+                        current_block.children.push(new_parent)
+                        current_block.children.sort(function(a, b) {
+                            return a.id.toString().localeCompare(b.id.toString())
+                        })
+                        current_block = new_parent
+                    } else {
+                        current_block = current_block.children[index]
+                    }
+                }
+                if (new_id) {
+                    let merge_index = current_block.children.findIndex(v => v.id == new_id)
+                    if (merge_index > -1) {
+                        for (let i = 0; i < element.children.length; i++) {
+                        this.add_leaf(localValue, scope, element.children[i])
+                        }
+                    } else {
+                        let new_element = Object.assign({}, element)
+                        new_element.id = new_id
+                        current_block.children.push(new_element)
+                        current_block.children.sort(function(a, b) {
+                            return a.id.toString().localeCompare(b.id.toString())
+                        })
+                    }
+                    return
+                }
+                current_block.children.push(element)
+                current_block.children.sort(function(a, b) {
+                    return a.id.toString().localeCompare(b.id.toString())
+                })
+            },
+            /**
+             * Removes element from a tree (localValue).
+             * When searching for the element, all its parents are stored
+             * If the element is a leaf and parent does not have more children, any and all empty parents are removed
+             * @param localValue Tree where element is being added to
+             * @param scope Scope name corresponding to tree
+             * @param element Element being added to the tree (localValue)
+             */
+            remove_leaf: function(localValue, scope, element) {
+                let split
+                let parents = []
+                if (Object.prototype.hasOwnProperty.call(element, 'field_name')) {
+                    split = element.field_name.split('.')
+                } else {
+                    split = element.id.split('.')
+                    split.splice(0, 1)
+                }
+
+                let current_name = scope + '.' + split[0]
+                let first_index = localValue.findIndex(v => v.id == current_name)
+                let current_block = localValue[first_index]
+                parents.push(localValue[first_index])
+
+                for (let i = 1; i < split.length - 1; i++) {
+                    current_name += '.' + split[i]
+                    let index = current_block.children.findIndex(v => v.id == current_name)
+                    parents.push(current_block.children[index])
+                    current_block = current_block.children[index]
+                }
+                current_name += '.' + split[split.length - 1]
+                let leaf_index = current_block.children.findIndex(v => v.field_name == split.join('.') || v.id == current_name)
+                current_block.children.splice(leaf_index, 1)
+
+                this.remove_empty_parents(parents)
+            },
+            /**
+             * Removes empty parents after last leaf removal
+             * @param parents Array of parent objects to check for emptiness
+             */
+            remove_empty_parents(parents) {
+                for (let i = parents.length - 1; i > 0; i--) {
+                    if (parents[i].children.length == 0) {
+                        let remove_index = parents[i - 1].children.findIndex(v => v == parents[i])
+                        parents[i - 1].children.splice(remove_index, 1)
+                    }
+                }
+            },
+        },
+        watch: {
+            'current_study.mappings': function() {
+                this.new_fields_tree()
+            },
+            ready: function() {
+                this.new_fields_tree()
+            },
+            tree_list: function() {
+                this.$emit('change_tree', this.tree_list)
+            }
+        },
+        mounted() {
+            this.get_scopes()
+            this.get_classes()
+            this.get_fields()
+        },
+        computed: {
+            ready: function() {
+                return this.scopes && this.classes && this.fields
+            }
+        },
+    }
+</script>
