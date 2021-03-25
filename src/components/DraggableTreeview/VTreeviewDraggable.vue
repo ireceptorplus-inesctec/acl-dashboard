f64009ec119b3c4cd61d5ecdcd98580fbac304e3
Edgar Silva <23esms23@gmail.com>
2021-03-25 10:49:24 +0000

    treeview draggable components

---
+++ src/components/DraggableTreeview/VTreeviewDraggable.vue
@@ -0,0 +1,134 @@
+<template>
+    <draggable
+        :value="localValue"
+        :group="group"
+        class="v-treeview v-treeview-draggable"
+        :class="themeClassName"
+        ghost-class="ghost"
+        @change="changed"
+    >
+        <v-treeview-draggable-node
+            v-for="item in value"
+            :key="item.id"
+            :group="group"
+            :value="item"
+            :root_node="localValue"
+            :changed="changed"
+        >
+            <template v-if="hasSlotPrepend" v-slot:prepend="{ item, open }">
+                <slot name="prepend" v-bind="{ item, open }"></slot>
+            </template>
+            <template v-slot:label="{ item, open }">
+                <slot name="label" v-bind="{ item, open }"></slot>
+            </template>
+            <template v-if="hasSlotAppend" v-slot:append="{ item, open }">
+                <slot name="append" v-bind="{ item, open }"></slot>
+            </template>
+        </v-treeview-draggable-node>
+    </draggable>
+</template>
+
+<script>
+    // import Vue, { PropType } from "vue";
+    import draggable from "vuedraggable";
+    import VTreeviewDraggableNode from "./VTreeviewDraggableNode.vue";
+    export default {
+        components: {
+            draggable,
+            VTreeviewDraggableNode
+        },
+        props: {
+            value: {
+                type: Array,
+                // type: Array as PropType<{ id: string | number }[]>,
+                default: function () {
+                    return []
+                }
+                // default: (): { id: string | number }[] => {
+                //    return [];
+                // }
+            },
+            group: {
+                type: String,
+                default: null
+            },
+            scope: {
+              type: String,
+              default: null
+            },
+            add_leaf: {
+              type: Function,
+              default: function() {}
+            },
+            remove_leaf: {
+              type: Function,
+              default: function() {}
+            },
+        },
+        data() {
+            return {
+                localValue: [...this.value],
+            };
+        },
+        computed: {
+          themeClassName: function () {
+            return this.$vuetify.theme.isDark ? "theme--dark" : "theme--light";
+          },
+
+          hasSlotPrepend () {
+            return !!this.$scopedSlots['prepend'] || !!this.$slots['prepend'];
+          },
+
+          hasSlotAppend () {
+            //let result = !!this.$slots['append'] || !!this.$scopedSlots['append'];
+//            console.log('append-root', !!this.$scopedSlots['append']);
+            return !!this.$scopedSlots['append'] || !!this.$slots['append'] ;
+          },
+        },
+        watch: {
+            value(value) {
+//                console.log('WatchValue/DraggableTreeView: this.localValue', this.localValue, 'value', value);
+                this.localValue = [...value];
+            }
+        },
+        methods: {
+            // updateValue: function (value) {
+            //     this.localValue = value;
+            //     this.$emit("input", this.localValue);
+            //     console.log('updateValue: ')
+            //     console.log(value)
+            // },
+            // updateItem: function (itemValue) {
+            //     const index = this.localValue.findIndex(v => v.id === itemValue.id);
+            //     this.$set(this.localValue, index, itemValue);
+            //     this.$emit("input", this.localValue);
+            //     console.log('updateItem - itemValue: ')
+            //     console.log(itemValue)
+            //     console.log('updateItem - localValue: ')
+            //     console.log(this.localValue)
+            // },
+            changed: function(changed) {
+              if (changed.added) {
+                if (this.check_has_parent(changed.added.element)) {
+                  this.add_leaf(this.localValue, this.scope, changed.added.element)
+                } else {
+                  this.localValue.push(changed.added.element)
+                  this.$emit("input", this.localValue)
+                }
+              } else if (changed.removed) {
+                if (this.check_has_parent(changed.removed.element)) {
+                  this.remove_leaf(this.localValue, this.scope, changed.removed.element)
+                } else {
+                  let index = this.localValue.findIndex(v => v.id == changed.removed.element.id)
+                  this.localValue.splice(index, 1)
+                  this.$emit("input", this.localValue)
+                }
+              }
+            },
+            check_has_parent: function(element) {
+              return (Object.prototype.hasOwnProperty.call(element, 'field_name') && element.field_name.split('.').length > 1)
+                || element.id.split('.').length > 1
+            },
+        }
+    }
+</script>
---
+++ src/components/DraggableTreeview/VTreeviewDraggableNode.vue
@@ -0,0 +1,168 @@
+<template>
+    <div :class="['v-treeview-node v-treeview-node--click', {'v-treeview-node--leaf' : !hasChildren}]">
+        <div class="v-treeview-node__root" @click="open = !open">
+            <div v-for="levelNr in levelsToAdd" :key="levelNr" class="v-treeview-node__level"></div>
+            <button v-if="hasChildren" type="button"  class="v-icon notranslate v-treeview-node__toggle v-icon--link mdi mdi-menu-down"
+                :class="{
+                      'v-treeview-node__toggle--open': open,
+                      'theme--dark': isDark,
+                      'theme--light': !isDark
+                    }"
+            />
+            <div class="v-treeview-node__content">
+                <div v-if="hasSlotPrepend" class="v-treeview-node__prepend">
+                    <slot name="prepend" v-bind="{ item: value, open }" />
+                </div>
+                <div class="v-treeview-node__label">
+                    <slot name="label" v-bind="{ item: value, open }">
+                        {{ value.name }}
+                    </slot>
+                </div>
+                <div v-if="hasSlotAppend" class="v-treeview-node__append">
+                    <slot name="append" v-bind="{ item: value, open }" />
+                </div>
+            </div>
+        </div>
+        <div v-if="hasChildren && open" class="v-treeview-node__children v-treeview-node__children__draggable">
+            <draggable
+                :group="group"
+                :value="value.children"
+                ghost-class="ghost"
+                @change="changed"
+            >
+                <v-treeview-draggable-node
+                    v-for="child in value.children"
+                    :key="child.id"
+                    :group="group"
+                    :level="level+1"
+                    :value="child"
+                    :root_node="root_node"
+                    :changed="changed"
+                >
+                    <template v-if="hasSlotPrepend" v-slot:prepend="{ item, open }">
+                        <slot name="prepend" v-bind="{ item, open }" />
+                    </template>
+                    <template v-slot:label="{ item, open }">
+                        <slot name="label" v-bind="{ item, open }" />
+                    </template>
+                    <template v-if="hasSlotAppend" v-slot:append="{ item, open }">
+                        <slot name="append" v-bind="{ item, open }" />
+                    </template>
+                </v-treeview-draggable-node>
+            </draggable>
+        </div>
+    </div>
+</template>
+
+<script>
+    import draggable from "vuedraggable";
+    /*
+    type TreeItem = {
+        id: number;
+        name: string;
+        children: TreeItem[];
+    };
+    */
+    export default {
+        name: "VTreeviewDraggableNode",
+        components: {
+            draggable
+        },
+        props: {
+            value: {
+                type: Object,
+                // type: Object as PropType<TreeItem>,
+                default: function () {
+                    return {
+                        id: 0,
+                        name: "",
+                        children: []
+                    }
+                }
+            },
+            root: {
+                type: Boolean,
+                default: function () {
+                  return false;
+                }
+            },
+            level: {
+              type: Number,
+              default: function () {
+                return 0;
+              }
+            },
+            group: {
+                type: String,
+                default: null
+            },
+            root_node: {
+                type: Array,
+                // type: Object as PropType<TreeItem>,
+                default: function () {
+                    return []
+                }
+            },
+            changed: {
+              type: Function,
+              default: function () {
+                return () => {}
+              }
+            },
+        },
+        data() {
+            return {
+                open: false,
+                localValue: { ...this.value }
+            };
+        },
+        computed: {
+            hasChildren: function () {
+                return this.value.children != null && this.value.children.length > 0;
+            },
+            hasSlotPrepend () {
+              return !!this.$scopedSlots['prepend'] || !!this.$slots['prepend'];
+            },
+            hasSlotAppend () {
+              //let result = !!this.$slots['append'] || !!this.$scopedSlots['append'];
+//              console.log('append', !!this.$scopedSlots['append']);
+              return !!this.$slots['append'] || !!this.$scopedSlots['append'];
+            },
+            isDark: function () {
+                return this.$vuetify.theme.isDark;
+            },
+            levelsToAdd: function () {
+              return this.level + (this.hasChildren ? 0 : 1);
+            },
+        },
+        watch: {
+            value: function (value) {
+//              console.log('WatchValue/DraggableTreeViewNODE: this.localValue', this.localValue, 'value', value);
+              this.localValue = { ...value };
+            }
+            /*
+            value(value): void {
+                this.localValue = { ...value };
+            }
+            */
+        },
+        methods: {
+            // updateValue: function (value) {
+            //     console.log('Node: updateValue', value)
+            //     this.localValue.children = [...value];
+            //     this.$emit("input", this.localValue);
+            // },
+            // updateChildValue: function (value) {
+            //     console.log('Node: updateChildValue', value);
+            //     const index = this.localValue.children.findIndex(c => c.id === value.id);
+            //     this.$set(this.localValue.children, index, value);
+            //     this.$emit("input", this.localValue);
+            // },
+        },
+        mounted() {
+            if (this.value.is_open) {
+                this.open = true
+            }
+        }
+    };
+</script>
