9c8cf1480da813422486126c0ebaaf240b6aeb69
Edgar Silva <23esms23@gmail.com>
2021-05-31 18:07:53 +0100

    template picker

--- src/components/StudyManagement/TemplatePicker.vue
+++ src/components/StudyManagement/TemplatePicker.vue
@@ -1,21 +1,59 @@
 <template>
     <div v-if="is_study">
-        <v-select
-            v-model="template"
-            :items="templates_names"
-            dark
-            label="Template"
-            class="selectable"
-        >
-        </v-select>
-        <new-template-dialog :open="new_template_open" v-on:close="new_template_close"></new-template-dialog>
+        <v-row>
+            <v-col
+             cols="12"
+             sm="6"
+             md="10">
+                <v-select
+                 v-model="template"
+                 :items="templates_names"
+                 :dark="(mode === 'dark')"
+                 label="Template"
+                 class="selectable"
+                >
+                </v-select>
+            </v-col>
+            <v-col
+             cols="6"
+             md="1">
+                <v-btn
+                 class="mx-2 delIcon"
+                 fab
+                 :dark="(mode === 'dark')"
+                 small
+                 v-on:click="save_template">
+                    <v-icon
+                     :dark="(mode === 'dark')"
+                    >
+                        mdi-floppy
+                    </v-icon>
+                </v-btn>
+            </v-col>
+            <v-col
+             cols="6"
+             md="1">
+                <v-btn
+                 class="mx-2 delIcon"
+                 fab
+                 :dark="(mode === 'dark')"
+                 small
+                 v-on:click="delete_template">
+                    <v-icon
+                     :dark="(mode === 'dark')"
+                    >
+                        mdi-delete
+                    </v-icon>
+                </v-btn>
+            </v-col>
+        </v-row>
+        <new-template-dialog :open="new_template_open" v-on:close="new_template_close" v-on:input="new_template_close"></new-template-dialog>
     </div>
 </template>
 
 <script>
 import NewTemplateDialog from "./NewTemplateDialog"
 const axios = require('axios')
-const new_template = '+ save new template'
     export default {
         name: 'TemplatePicker',
         components: {
@@ -45,8 +83,6 @@ const new_template = '+ save new template'
                     let index = this.templates_names.indexOf(val)
                     if (this.templates[index]) {
                         this.$emit('select_template', this.templates[index].id)
-                    } else {
-                        this.new_template_open = true
                     }
                 }
             },
@@ -87,13 +123,16 @@ const new_template = '+ save new template'
                     names.push(templates[i].name)
                 }
                 this.templates_names = names
-
-                this.templates_names.push(new_template)
             },
             new_template_close(name) {
-                this.create_template(name, this.get_mappings_from_tree())
+                if (name != null) {
+                    this.create_template(name, this.get_mappings_from_tree())
+                }
                 this.new_template_open = false
                 this.get_templates()
+            },
+            save_template() {
+                this.new_template_open = true
             },
             create_template(name, mappings) {
                 let post_template = {
@@ -109,6 +148,30 @@ const new_template = '+ save new template'
                 axios.post(url, post_template, config)
                 .then((response) => {
                     console.log(response)
+                    this.get_templates()
+                })
+                .catch((e) => {
+                    console.log(e)
+                })
+            },
+            delete_template() {
+                if (!confirm("Do you really want to delete?"))
+                    return
+                var filtered = this.templates.filter(e => e.name == this.template)
+                if (filtered.length < 0) {
+                    console.log('template not found')
+                    return
+                }
+                let url = process.env.VUE_APP_MAPPINGS_BASE_PATH + 'templates/' + filtered[0].id
+                let config = {
+                    headers: {
+                        'Authorization': 'Bearer ' + localStorage.getItem('access-token')
+                    }
+                }
+                axios.delete(url, config)
+                .then((response) => {
+                    console.log(response)
+                    this.get_templates()
                 })
                 .catch((e) => {
                     console.log(e)
@@ -118,5 +181,10 @@ const new_template = '+ save new template'
         mounted() {
             this.get_templates()
         },
+        computed: {
+            mode: function() {
+                return this.$store.state.mode
+            }
+        }
     }
 </script>
